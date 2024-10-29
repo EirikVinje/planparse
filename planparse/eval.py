7b54be3f58f0322a06c620449a8ac8bfba9b0bce
Eirik Vinje <eirik.matias@gmail.com>
2024-10-29 12:31:14 +0100

    add skeleton files to implement

--- planparse/eval.py
+++ planparse/eval.py
@@ -1,3 +1,23 @@
 import argparse
 
 
+
+def evaluate():
+    pass
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", type=str, default=None, help="Path to pdf file(s) to evaluate")
+    args = parser.parse_args()
+
+    paths = args.input.split(",")
+    
+
