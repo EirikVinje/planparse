8a1d29ad001c72fd9ae4cf463684fb4da9384fca
Eirik Vinje <eirik.matias@gmail.com>
2024-10-29 13:20:01 +0100

    add argparse eval.py

--- planparse/eval.py
+++ planparse/eval.py
@@ -15,9 +15,17 @@ def evaluate():
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input", type=str, default=None, help="Path to pdf file(s) to evaluate")
+    parser.add_argument("--input", type=str, default=None, help="Path to pdf file(s) to evaluate. e.g ./ex_data/200921.pdf,./ex_data/150326.pdf")
+    parser.add_argument("--model", type=str, default=None, help="Path to model. e.g ./models/norwai-norwai-mistral-7b-instruct-20230414162541")
     args = parser.parse_args()
 
     paths = args.input.split(",")
     
+    # load model
+
+    # load data
+
+    # generate llm data
+
+    # evaluate
 
